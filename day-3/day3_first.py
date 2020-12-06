2b90ec7b9b110ed947eb2811fd1b8a87989b764b
mchlr <mrenn2000@yahoo.de>
2020-12-06 18:32:03 +0100

    Solved day3 part 1

---
+++ day-3/day3_first.py
@@ -0,0 +1,30 @@
+import os
+import math
+import re
+
+
+def load_challenge():
+    p = os.path.join(os.path.dirname(__file__), 'challenge.txt')
+    cont = open(p)
+    return list(cont.read().split("\n"))
+    
+def solve(map):
+    tree_count = 0
+    xindex = 0
+    for line in map:
+        arr = list(line)
+        idx = (xindex % len(arr))
+
+        if(arr[idx] == "#"):
+            tree_count += 1
+        xindex += 3
+
+    return tree_count 
+
+def main():
+    sol = solve(load_challenge())
+    print("Trees hit: ", sol)
+
+
+# Execute
+main()
