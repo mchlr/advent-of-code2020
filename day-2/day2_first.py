01a8817bcb8869e12293657b3675c05d2feca631
mchlr <mrenn2000@yahoo.de>
2020-12-06 16:15:34 +0100

    Solved day2 part 1

--- day-2/day2_first.py
+++ day-2/day2_first.py
@@ -1,7 +1,7 @@
 import os
 import math
+import re
 
-valid_count = 0
 
 def load_challenge():
     p = os.path.join(os.path.dirname(__file__), 'challenge.txt')
@@ -9,13 +9,30 @@ def load_challenge():
     return list(cont.read().split("\n"))
     
 def solve(pws):
+    valid_count = 0
     for pw in pws:
-        # do stuff here
-        return 0
+        matches = re.split(r"(\d*-\d*).(\w): (\w*)", pw)
+        # Process the min/max group
+        tmp = matches[1].split("-")
+
+        if(verify_password(tmp[0], tmp[1], matches[2], matches[3])):
+            valid_count += 1
+            
+    return valid_count
+
+def verify_password(min, max, char, pw):
+    count = 0
+    for x in pw:
+        if(x == char):
+            count += 1
+
+    f = count >= int(min) and count <= int(max)
+    return f
+
 
 def main():
-    solve(load_challenge())
-    print("Passwords Valid: ", valid_count)
+    sol = solve(load_challenge())
+    print("Passwords Valid: ", sol)
 
 
 # Execute
